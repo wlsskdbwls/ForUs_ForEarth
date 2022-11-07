b5cbd19a73f77bbf5d5a6f3d26a8ac824b7f41a3
akrrkdgksdywjd <106609216+akrrkdgksdywjd@users.noreply.github.com>
2022-11-08 01:16:45 +0900

    회원가입 & 로그인 기능

--- app/src/main/java/com/example/forusforearth/MainActivity.kt
+++ app/src/main/java/com/example/forusforearth/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.forusforearth
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var auth : FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+
+        // 로그아웃
+        logoutbutton.setOnClickListener {
+            // 로그아웃하고 로그인 화면(SigninActivity)로 이동
+            val intent = Intent(this, SigninActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+            auth?.signOut()
+        }
     }
 }
---
+++ app/src/main/java/com/example/forusforearth/signup.kt
@@ -0,0 +1,56 @@
+package com.example.forusforearth
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_signup.*
+
+class  SignupActivity : AppCompatActivity() {
+    private var auth : FirebaseAuth? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+        auth = Firebase.auth
+
+        //로그인 화면으로 돌아가기
+        imgSignUpBack.setOnClickListener {
+            startActivity(Intent(this,SigninActivity::class.java))
+        }
+
+
+        // 계정 생성 버튼
+        btnSignUpNext.setOnClickListener {
+            createAccount(edtSignUpID.text.toString(),edtSignUpPwd.text.toString())
+        }
+    }
+
+    // 계정 생성
+    private fun createAccount(email: String, password: String) {
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth?.createUserWithEmailAndPassword(email, password)
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            this, "계정 생성 완료.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        finish() // 가입창 종료
+                    } else {
+                        Toast.makeText(
+                            this, "계정 생성 실패",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
+    }
+}
+
+
+
+

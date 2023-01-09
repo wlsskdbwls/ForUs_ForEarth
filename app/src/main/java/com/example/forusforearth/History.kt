6c72f3735a33fdd46137ddbfc5495ffcff2e2776
akrrkdgksdywjd <106609216+akrrkdgksdywjd@users.noreply.github.com>
2023-01-09 16:55:42 +0900

    히스토리 & 스탬프 xml

---
+++ app/src/main/java/com/example/forusforearth/History.kt
@@ -0,0 +1,15 @@
+package com.example.forusforearth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+
+class History : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+
+
+    }
+}
